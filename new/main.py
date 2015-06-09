4f8689e82f9c25e02a38e75a60b67c03287c6a6d
root <root@debian-imen>
2015-06-09 23:05:33 +0100

    test liste op

--- new/main.py
+++ new/main.py
@@ -5,6 +5,7 @@ import sys
 from PyQt5 import QtWidgets
 from ajouter_emp import Ajouter_Employe
 from liste_employe import Liste_Employe
+from liste_op import Liste_Operation
 from login import Login
 import connexion
 
@@ -13,8 +14,9 @@ if __name__ == "__main__":
 
     app = QtWidgets.QApplication(sys.argv)
     connexion.connect()
+    MainWindow = Liste_Operation()
     #MainWindow = Liste_Employe()
     #MainWindow = Ajouter_Employe()
-    MainWindow = Login()
+    #MainWindow = Login()
     MainWindow.show()
     sys.exit(app.exec_())
